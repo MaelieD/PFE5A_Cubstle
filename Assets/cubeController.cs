75a30a60462f121d873a50b6aa7a53a12b4e96d8
lehmstedt <lehmstedt-sebastien@hotmail.fr>
2016-11-01 22:16:54 +0100

    Placement des cubes sensible à la hauteur de la souris

--- Assets/cubeController.cs
+++ Assets/cubeController.cs
@@ -1,29 +1,23 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 
 
 
 public class cubeController : MonoBehaviour {
-	int m_i;
-	float m_cubeLenght;
 	float m_cubeHeight;
+	List<GameObject> m_cubeList;
 
-	public int g_wallSize;
-	public float g_dropHigh;
 	public float g_mass;
 	public float g_gap;
 	public float g_offset;
 	public float g_sideOffset;
 	public GameObject g_unitCube;
+	public GameObject g_currentCube;
 
 	// Use this for initialization
 	void Start () {
-		//itérateur de blocs du mur
-		m_i = 0;
-		//taille du mur
-		g_wallSize = 20;
-		//hauteur à partir de laquelle sont lachés les blocs du mur
-		g_dropHigh = 0.3f;
+
 		//masse des blocs
 		g_mass = 1000.0f;
 		//écart minimal entre deux blocs du mur
@@ -32,54 +26,82 @@ public class cubeController : MonoBehaviour {
 		g_offset = 0.7f;
 		//décalage de côté pour les blocs
 		g_sideOffset = 0.2f;
+		//liste de tous les cubes posés
+		m_cubeList = new List<GameObject> ();
 
 		g_unitCube = GameObject.Find ("Unit Cube");
-		m_cubeLenght = g_unitCube.transform.localScale.x;
 		m_cubeHeight = g_unitCube.transform.localScale.y;
+
 	
 	}
 	
 	// Update is called once per frame
 	void Update () {
 
-		//pour créer le mur
-		if (Input.GetKey ("left ctrl")) {
-			float randomizedGap = Random.Range (-g_gap, g_gap);
-			float randomizedSideOffset = Random.Range (-g_sideOffset, g_sideOffset);
-			createCube (new Vector3 ((m_i % g_wallSize) * (m_cubeLenght + 2 * g_gap) + randomizedGap + g_offset * (m_i / g_wallSize % 2), ((m_i / g_wallSize) + m_cubeHeight) + g_dropHigh, randomizedSideOffset));
-			m_i++;
-		}
-
 		if(Input.GetMouseButtonDown(0)){
-			var mousePos = Input.mousePosition;
-			mousePos.z = 5.0f;
-			mousePos = Camera.main.ScreenToWorldPoint (mousePos);
-			mousePos.y = g_dropHigh + m_cubeHeight;
-			float yAngle = transform.eulerAngles.y;
-			Quaternion cubeQuat = Quaternion.Euler (new Vector3 (0.0f, yAngle, 0.0f));
-		
-			createCube (mousePos, cubeQuat);
+			Vector3 mousePos = getMousePos ();
+
+			createCube (mousePos);
 		}
 
+		//pour placer un bloc
+		if(Input.GetMouseButton(0)){
+			Vector3 mousePos = getMousePos ();
+
+			moveCube (mousePos);
+
+		}
+		if(Input.GetMouseButtonUp(0)){
+			placeCube ();
+		}
+
+		if(Input.GetKeyDown("p")){
+			enterPlayMode ();
+		}
 	
 	}
 
 	void createCube(Vector3 p_pos){
-		GameObject cube = Instantiate (g_unitCube);
-		Rigidbody rigidBody = cube.GetComponent<Rigidbody> ();
-		cube.transform.position = p_pos;
+		g_currentCube = Instantiate (g_unitCube);
+		g_currentCube.name = "Current Cube";
+		Rigidbody rigidBody = g_currentCube.GetComponent<Rigidbody> ();
+		g_currentCube.transform.position = p_pos;
 		Debug.Log (p_pos);
 		rigidBody.mass = g_mass;
+		rigidBody.isKinematic = false;
+		rigidBody.MovePosition (p_pos);
+		rigidBody.constraints = RigidbodyConstraints.FreezeRotation;
 	}
 
-	void createCube(Vector3 p_pos, Quaternion p_quat){
-		GameObject cube = Instantiate (g_unitCube);
-		Rigidbody rigidBody = cube.GetComponent<Rigidbody> ();
-		cube.transform.position = p_pos;
-		cube.transform.rotation = p_quat;
-		Debug.Log (p_pos);
-		rigidBody.mass = g_mass;
+	Vector3 getMousePos(){
+		var mousePos = Input.mousePosition;
+		mousePos.z = 3.0f;
+		mousePos = Camera.main.ScreenToWorldPoint (mousePos);
+		if(mousePos.y < m_cubeHeight / 2){
+			mousePos.y = m_cubeHeight / 2;
+		}
+		return mousePos;
+	}
+		
+
+	void moveCube(Vector3 p_pos){
+		g_currentCube.transform.position = p_pos;
 	}
 
+	void placeCube(){
+		g_currentCube.name = "Placed Cube";
+		g_currentCube.GetComponent<cubeBehaviour> ().g_isPlaced = true;
+		g_currentCube.GetComponent<Rigidbody> ().constraints = RigidbodyConstraints.FreezeAll;
+		m_cubeList.Add (g_currentCube);
+
+	}
+
+	void enterPlayMode(){
+		Debug.Log ("entered Play Mode");
+
+		foreach(GameObject gameObject in m_cubeList){
+			gameObject.GetComponent<Rigidbody> ().constraints = RigidbodyConstraints.None;
+		}
+	}
 		
 }
