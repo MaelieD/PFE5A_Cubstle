d823c7091812de84b27e16361536082b3b7fca20
lehmstedt <lehmstedt-sebastien@hotmail.fr>
2016-11-02 18:14:49 +0100

    Modification de la distance joueur/cube avec la molette souris

--- Assets/cubeController.cs
+++ Assets/cubeController.cs
@@ -8,9 +8,7 @@ public class cubeController : MonoBehaviour {
 	float m_cubeHeight;
 	List<GameObject> m_cubeList;
 
-	public float g_mass;
-
-
+	public float g_cubeDistance;
 	public GameObject g_unitCube;
 	public GameObject g_currentCube;
 	public Rigidbody g_currentRigidbody;
@@ -18,14 +16,17 @@ public class cubeController : MonoBehaviour {
 	// Use this for initialization
 	void Start () {
 
-		//masse des blocs
-		g_mass = 1000.0f;
-
 		//liste de tous les cubes posés
 		m_cubeList = new List<GameObject> ();
 
+		//cube unitaire sur lequel on clone les blocs
 		g_unitCube = GameObject.Find ("Unit Cube");
+
+		//hauteur d'un bloc
 		m_cubeHeight = g_unitCube.transform.localScale.y;
+
+		//distance entre le joueur et le cube
+		g_cubeDistance = 3.0f;
 
 	
 	}
@@ -53,6 +54,15 @@ public class cubeController : MonoBehaviour {
 		if(Input.GetKeyDown("p")){
 			enterPlayMode ();
 		}
+
+		if(Input.GetAxis("Mouse ScrollWheel") > 0.0f){
+			g_cubeDistance++;
+		}
+		if(Input.GetAxis("Mouse ScrollWheel") < 0.0f){
+			g_cubeDistance--;
+		}
+
+		g_cubeDistance = Mathf.Clamp (g_cubeDistance, 2.0f, 15.0f);
 	
 	}
 
@@ -64,16 +74,13 @@ public class cubeController : MonoBehaviour {
 		g_currentRigidbody = g_currentCube.GetComponent<Rigidbody> ();
 		g_currentCube.transform.position = p_pos;
 		Debug.Log (p_pos);
-		g_currentRigidbody.mass = g_mass;
-		g_currentRigidbody.isKinematic = false;
 		g_currentRigidbody.MovePosition (p_pos);
-		g_currentRigidbody.constraints = RigidbodyConstraints.FreezeRotation;
 		g_currentRigidbody.useGravity = false;
 	}
 
 	Vector3 getMousePos(){
 		var mousePos = Input.mousePosition;
-		mousePos.z = 3.0f;
+		mousePos.z = g_cubeDistance;
 		mousePos = Camera.main.ScreenToWorldPoint (mousePos);
 		if(mousePos.y < m_cubeHeight / 2){
 			mousePos.y = m_cubeHeight / 2;
